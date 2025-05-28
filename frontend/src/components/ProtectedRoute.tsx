0771cfb0efe84948014412a0e9db07789189c5cc
OmRana2004 <omrana8755800322@gmail.com>
2025-05-28 23:46:43 +0530

    clear

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -8,4 +8,4 @@ const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
 
 export default ProtectedRoute;
 
-// new this is a simple protected route component that checks if the user is an admin by looking for a specific item in localStorage. If the user is an admin, it renders the children components; otherwise, it redirects to the admin page.
+
