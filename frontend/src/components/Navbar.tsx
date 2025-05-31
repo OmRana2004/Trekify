aadef45fd99ae99199fea7c498cb46ce6a990622
OmRana2004 <omrana8755800322@gmail.com>
2025-05-31 23:58:45 +0530

    ./git/COMMIT_EDITMSG

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -216,6 +216,3 @@ const Navbar: React.FC = () => {
 };
 
 export default Navbar;
-// Compare this snippet from frontend/src/components/Navbar.tsx:
-// import React, { useState, useEffect, useRef } from "react";./
-// import { NavLink } from "react-router-dom";
