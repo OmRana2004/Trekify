7ff63c51f7ed86072f31871e1ebd09d996acba87
OmRana2004 <omrana8755800322@gmail.com>
2025-07-03 22:52:35 +0530

    ./git/COMMIT_EDITMSG

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -325,4 +325,4 @@ const Home = () => {
 
 export default Home;
 // add some important comments here
-// This Home component serves as the main landing page for the Trekify application.
+// This Home component serves as the main landing page for the 
